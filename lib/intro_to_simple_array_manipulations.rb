dccc198aae08b95f63db41d9ff72343066918bce
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-08 01:46:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,6 @@ def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
 
-def using_delete(instructors, "Steven")
-  instructors.delete_if("Steven")
+def using_delete(instructors, string)
+  instructors.delete_if {string == "Steven"}
 end
