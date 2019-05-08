4f7b94ad6cbaaaf7cd9e9a915ad6e16da4648979
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-08 00:55:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,7 @@
 def using_push(countries_in_western_africa, next_country)
   countries_in_western_africa.push(next_country)
 end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, @updated_array)
+  neighborhoods_in_northwest_brooklyn.unshift(@updated_array)
+end
